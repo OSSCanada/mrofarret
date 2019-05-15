e9adc8e73b8343c55c132c1a58a654cb76361a42
Raymond Kao <rakao@microsoft.com>
2019-05-15 14:08:45 -0400

    really early days

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const ClientID = process.env.ARM_CLIENT_ID;
 const ClientSecret = process.env.ARM_CLIENT_SECRET;
 const SubscriptionID = process.env.ARM_SUBSCRIPTION_ID;
 const TenantID = process.env.ARM_TENANT_ID;
+const debug = process.env.DEBUG || false;
 const FS = require('fs');
 
-
-function getCredentials() {
+function getCredentials() { 
   return new Promise((resolve, reject) => {
     MsRest.loginWithServicePrincipalSecret(
       ClientID,
@@ -21,12 +21,49 @@ function getCredentials() {
   })
 }
 
+function toTfObject(azObject){
+  const startTF = `resource "${azObject.armResourceType}" "${azObject.name}" {`
+  const endTF = `}`
+  let tfObject = startTF + "\n";
+
+  delete azObject.id;
+  delete azObject.armResourceType;
+  delete azObject.managedBy;
+  
+  for (var property in azObject) {   
+    const type = azObject[property].constructor;
+
+    if(azObject.hasOwnProperty(property)){
+      switch(type) {
+        case Object:
+          break;
+        default:
+          tfObject += objectify(property, azObject[property])
+      }
+    }
+  }
+
+  tfObject += endTF;
+
+  if(debug){
+    console.log(tfObject);
+  }
+
+  return tfObject;
+}
+
+function objectify(property, value) {
+  return "\t" + `${property} = "${value}"` + "\n";
+}
+
 getCredentials().then(credentials => {
   const client = new AzureResources.ResourceManagementClient(credentials, SubscriptionID);
 
-  client.resources.list()
-  .then(result => {
-    console.log(result.length)    
+  client.resourceGroups.list()
+  .then((groups)=>{
+    groups.forEach((group, index) => {
+      group.armResourceType = 'azurerm_resource_group';
+      toTfObject(group);
+    })
   })
-  
 })
