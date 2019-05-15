7c3e8484052dd4a09081030ac787e499d3bc026c
Raymond Kao <rakao@microsoft.com>
2019-05-15 15:34:36 -0400

    saving state

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const AzureCompute = require('@azure/arm-compute');
 const AzureResources = require('@azure/arm-resources')
 const MsRest = require('@azure/ms-rest-nodeauth');
+
+const FS = require('fs');
+
 const ClientID = process.env.ARM_CLIENT_ID;
 const ClientSecret = process.env.ARM_CLIENT_SECRET;
 const SubscriptionID = process.env.ARM_SUBSCRIPTION_ID;
 const TenantID = process.env.ARM_TENANT_ID;
-const debug = process.env.DEBUG || false;
-const FS = require('fs');
+
+
+const TFAzureResource = require('./lib/tfobj');
 
 function getCredentials() { 
   return new Promise((resolve, reject) => {
@@ -21,49 +25,21 @@ function getCredentials() {
   })
 }
 
-function toTfObject(azObject){
-  const startTF = `resource "${azObject.armResourceType}" "${azObject.name}" {`
-  const endTF = `}`
-  let tfObject = startTF + "\n";
-
-  delete azObject.id;
-  delete azObject.armResourceType;
-  delete azObject.managedBy;
-  
-  for (var property in azObject) {   
-    const type = azObject[property].constructor;
-
-    if(azObject.hasOwnProperty(property)){
-      switch(type) {
-        case Object:
-          break;
-        default:
-          tfObject += objectify(property, azObject[property])
-      }
-    }
-  }
-
-  tfObject += endTF;
-
-  if(debug){
-    console.log(tfObject);
-  }
-
-  return tfObject;
-}
-
-function objectify(property, value) {
-  return "\t" + `${property} = "${value}"` + "\n";
-}
-
 getCredentials().then(credentials => {
   const client = new AzureResources.ResourceManagementClient(credentials, SubscriptionID);
 
   client.resourceGroups.list()
   .then((groups)=>{
-    groups.forEach((group, index) => {
-      group.armResourceType = 'azurerm_resource_group';
-      toTfObject(group);
+    groups.forEach(group => {
+      let thing = new TFAzureResource('azurerm_resource_group', group);
+    
+      thing.saveToFile()
+        .then(obj => {console.log(obj)})
+        .catch()
     })
+    // groups.forEach((group, index) => {
+    //   group.armResourceType = 'azurerm_resource_group';
+    //   toTfObject(group);
+    // })
   })
 })
