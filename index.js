a56a821c89bba1ad66407d71cca27d7b5611f9a4
Raymond Kao <rakao@microsoft.com>
2019-05-16 00:58:42 -0400

    array will allow us to remove the leading whitespace when object is empty

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const AzureCompute = require('@azure/arm-compute');
 const AzureResources = require('@azure/arm-resources')
 const MsRest = require('@azure/ms-rest-nodeauth');
-
-const FS = require('fs');
 
 const ClientID = process.env.ARM_CLIENT_ID;
 const ClientSecret = process.env.ARM_CLIENT_SECRET;
@@ -25,27 +22,27 @@ function getCredentials() {
   })
 }
 
-getCredentials().then(credentials => {
+async function main() {
+  const credentials = await getCredentials();
   const client = new AzureResources.ResourceManagementClient(credentials, SubscriptionID);
-  
-  client.resourceGroups.list()
-  .then(async (groups)=>{
-    groups.forEach(async (group) => {
-      const resourceGroup = new TFAzureResource('azurerm_resource_group', group);
+  const groups = await client.resourceGroups.list()
     
-      resourceGroup.saveToFile()
+  groups.forEach(async (group) => {
+    const resourceGroup = new TFAzureResource('azurerm_resource_group', group);
+  
+    resourceGroup.saveToFile()
 
-      const resources = await client.resources.listByResourceGroup(group.name);
+    const resources = await client.resources.listByResourceGroup(group.name);
 
-      resources.forEach((resource) => {
-        resource.resource_group_name = "${" +  resourceGroup.terraformId + ".name}"
+    resources.forEach((resource) => {
+      resource.resource_group_name = "${" +  resourceGroup.terraformId + ".name}"
+    
 
-        const resourceObject = new TFAzureResource('azurerm_resource', resource);
+      const resourceObject = new TFAzureResource('azurerm_resource', resource);
 
-        resourceObject.saveToFile()
-          // .then(obj => console.log(obj))
-          // .catch(err => console.log(err))
-      })
+      return resourceObject.saveToFile()
     })
   })
-})
+}
+
+main();
